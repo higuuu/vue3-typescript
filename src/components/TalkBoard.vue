78faf2c1967791fded52f4a97fc31b8c8d0cd252
樋口修也 <higuzeroggw@gmail.com>
2021-05-22 13:52:20 +0900

    tsにして型推論をしてくれるように変更

--- src/components/TalkBoard.vue
+++ src/components/TalkBoard.vue
@@ -5,18 +5,18 @@
   </div>
 </template>
 
-<script>
+<script lang='ts'>
 import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
   name:'TalkBoard',
   props: {
     name: {
-      type: Number,
+      type: String,
       default: ''
     },
     message: {
-      type: Number,
+      type: String,
       default: ''
     }
   },
