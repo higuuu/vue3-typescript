20575ad5583a5a9631896f75ba72e75cf9e9c9ac
樋口修也 <higuzeroggw@gmail.com>
2021-05-22 16:54:56 +0900

    provide inject で設定変更の仕組みを作成

---
+++ src/components/DisplaySetting.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>
+    {{ displaySetting }}
+  </h2>
+</template>
+
+<script>
+import { inject, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "DisplaySetting",
+  setup() {
+    const displaySetting = inject("storeSetting");
+    return { displaySetting };
+  },
+});
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="changeSetting()">変更</button>
   </div>
+  <DisplaySetting />
 </template>
+
+<script>
+import { provide, defineComponent, ref } from "vue";
+import DisplaySetting from "@/components/DisplaySetting.vue";
+
+export default defineComponent({
+  components: {
+    DisplaySetting,
+  },
+  setup() {
+    const setting = ref("black");
+    function changeSetting() {
+      if (setting.value == "black") {
+        setting.value = "red";
+      } else {
+        setting.value = "black";
+      }
+    }
+    provide("storeSetting", setting);
+    return { changeSetting, setting };
+  },
+});
+</script>
