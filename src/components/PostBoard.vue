2e0156cdd322c52a71ec85d84b17e9bb364b93da
樋口修也 <higuzeroggw@gmail.com>
2021-05-22 15:51:30 +0900

    投稿フォームを作成しreactiveを活用してconsole.logで確認できるようにした

---
+++ src/components/PostBoard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+      <p>name</p>
+      <input v-model="post.name" label="name" />
+    </div>
+    <div>
+      <p>message</p>
+      <input v-model="post.message" label="message" />
+    </div>
+    <div>
+      <button @click="postAction()">送信</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+interface MessageProps {
+  name: string;
+  message: string;
+}
+
+export default defineComponent({
+  name: "PostBoard",
+  setup() {
+    const post = reactive<MessageProps>({
+      name: "",
+      message: "",
+    });
+    function postAction() {
+      console.log(post);
+    }
+    return { post, postAction };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,18 @@
   <div class="home" v-for="data in talkData" :key="data">
     <TalkBoard :name="data.name" :message="data.message" />
   </div>
+  <PostBoard />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import TalkBoard from "@/components/TalkBoard.vue"; // @ is an alias to /src
+import PostBoard from "@/components/PostBoard.vue";
 
 export default defineComponent({
   components: {
     TalkBoard,
+    PostBoard,
   },
   setup() {
     const talkData = [
