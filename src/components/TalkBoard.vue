30d00bc2e92b7e74d7b1e81dfbaeef9a801a5d90
樋口修也 <higuzeroggw@gmail.com>
2021-05-22 12:48:52 +0900

    コンポーネントをHomeに表示させる

---
+++ src/components/TalkBoard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Aさん</h2>
+    <p>メッセージ</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name:'TalkBoard'
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <TalkBoard />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import TalkBoard from "@/components/TalkBoard.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    TalkBoard,
   },
 })
 export default class Home extends Vue {}
